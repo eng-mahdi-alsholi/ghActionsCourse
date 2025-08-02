5a36037e04e2c9417d92b0ac5c12410f95db95c3
eng.mahdi.alsholi <eng.mahdi.alsholi@gmail.com>
2025-08-02 20:33:21 +0300

    Adds the test workflow

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,7 @@
 # This is a simple test script
 
 EXPECTED="Hello, World!"
-OUTPUT=$(node -e "console.log(require('./app')('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 if [ "$OUTPUT" = "$EXPECTED" ]; then
 
 echo "Test passed!"
